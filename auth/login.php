a3c0816b44acc81aea6dd090d184a5e2b6310778
huntznavarro <huntzorravan@gmail.com>
2026-05-08 16:01:48 +0800

    Initialize login page

--- auth/login.php
+++ auth/login.php
@@ -1,1 +1,10 @@
+<?php
 
+require_once __DIR__ . '/../includes/session.php';
+require_once __DIR__ . '/../config/db.php';
+
+
+if (isLoggedIn()) {
+    header('Location: /' . currentRole() . '/dashboard.php');
+    exit;
+}
